1b1cf57fe686f66a464b385834660c3e0df48694
Socks aka Janek <jan.kwiecien06@gmail.com>
2023-05-23 17:39:03 +0200

    :bug: non-interactive "urlopen(url).read()" wont work
    Ony thing that is read using this method is loading screen

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World")
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+url = "https://forms.office.com/Pages/ResponsePage.aspx?id=BTnzL-h5_kGxNuXPSTHur5MRzWm6IZ1DmCDtRDz55atUMU9NWEZERFZDN1c2RjBXVVBUUzFXUjRISy4u"
+html = urlopen(url).read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
+
+print(soup.get_text())
