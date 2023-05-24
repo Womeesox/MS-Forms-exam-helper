fcb94ae9f13d1e7ecd00b5b1b2d73950189a714d
Socks aka Janek <jan.kwiecien06@gmail.com>
2023-05-24 06:10:13 +0200

    :construction: Added working for user to log into.
    To change few things but it works.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
+import time
 
 
 url = "https://forms.office.com/e/jaTv4JAzPG"
@@ -17,16 +18,11 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
 driver.get(url)
 driver.maximize_window()
 
-#DOES NOT WAIT FOR URL TO CONTAIN GIVEN STRING
-WebDriverWait(driver, 10).until(EC.url_contains("forms.office.com"))
+#We have to wait on first operation cuz when login page is loading url actually contains "https://forms.office.com"
+while True:
+    time.sleep(1) #TO CHANGE ON FIRST ITERATION TO WAIT FOR LOADING PAGE NOT JUST 1 S
+    if driver.current_url.startswith("https://forms.office.com"):
+        time.sleep(2) #TO CHANGE TO WAIT FOR PAGE TO LOAD NOT JUST 2 S
+        break
 
-print("yeah babe")
-
-print(ChromeDriverManager().install())
-
-#Get html and parse it 
-
-# html = driver.page_source
-# soup = BeautifulSoup(html, "html.parser")
-
-# print(soup.get_text())
+#now get html and parse it 
